8b7b96fdcefceb7332346aec77691663c8c53242
nenyeonyema <chinenyeonyema1@gmail.com>
2024-05-20 15:41:38 -0700

    paginated csv file

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+""" Pagination """
+import csv
+from typing import List
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        """ Object constructor"""
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]  # Exclude header row
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Returns a page of the dataset."""
+        assert isinstance(page, int) and page > 0, "page must be a positive integer"
+        assert isinstance(page_size, int) and page_size > 0, "page_size must be a positive integer"
+
+        start_index, end_index = index_range(page, page_size)
+        dataset = self.dataset()
+
+        if start_index >= len(dataset):
+            return []
+
+        return dataset[start_index:end_index]
+
+
+if __name__ == "__main__":
+    server = Server()
+
+    try:
+        should_err = server.get_page(-10, 2)
+    except AssertionError:
+        print("AssertionError raised with negative values")
+
+    try:
+        should_err = server.get_page(0, 0)
+    except AssertionError:
+        print("AssertionError raised with 0")
+
+    try:
+        should_err = server.get_page(2, 'Bob')
+    except AssertionError:
+        print("AssertionError raised when page and/or page_size are not ints")
+
+    print(server.get_page(1, 3))
+    print(server.get_page(3, 2))
+    print(server.get_page(3000, 100))
