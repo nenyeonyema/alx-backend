6ab71b9a205e4435dbe5d9797f3be3f0c64daa68
nenyeonyema <chinenyeonyema1@gmail.com>
2024-05-31 05:31:12 -0700

    babel config

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Flask Babel """
+from flask import Flask, render_template
+from flask_bbabel import Babel
+
+
+class Config:
+    """ Configuration data """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@app_route('/')
+def index():
+    """ Renders a html page """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
